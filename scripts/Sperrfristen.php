bff9b7fa6ffeb8f647b676f1557b839afe4e7e8e
NEWUSER <ramin.khorsandi.90@gmail.com>
2019-09-26 14:48:23 +0200

    OPUSHOSTING-1152 Fixed date objects comparison

--- scripts/Sperrfristen.php
+++ scripts/Sperrfristen.php
@@ -3,27 +3,33 @@
 //This Script sets the unpublished Documents to published.
 //The script runs as a cron job.
 // Bootstrapping
-require_once dirname(__FILE__) . '/common/bootstrap.php';
+require_once dirname(__DIR__) . ('/common/bootstrap.php');
 
 $docFinder = new Opus_DocumentFinder();
 $docFinder->setServerState('unpublished');
 
-foreach ($docFinder->ids() as $id) {
+foreach ($docFinder->ids() as $id)
+{
 
     $d = null;
-    try {
+    try
+    {
         $d = new Opus_Document($id);
+        $originalDate = $d->getCompletedDate();
+
     }
-    catch (Opus_Model_NotFoundException $e) {
+    catch (Opus_Model_NotFoundException $e)
+    {
         // document with id $id does not exist
         continue;
     }
 
-    if (!is_null($d)) {
+    if (!is_null($d))
+    {
         $date = new Opus_Date();
-        $date->setNow();
-
-        if ((string)$d->getCompletedDate() <= (string)$date) {
+        $newDate = $date->setNow();
+        if ($originalDate <= $newDate)
+        {
             $d->setServerState('published');
             $d->setServerDatePublished($date);
             $d->store();
