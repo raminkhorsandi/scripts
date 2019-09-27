4c02a35a24338c2f7350b152dea931b982fee3ac
NEWUSER <ramin.khorsandi.90@gmail.com>
2019-09-27 13:45:44 +0200

    OPUSVIER-1152 modified the coding Style

--- scripts/Sperrfristen.php
+++ scripts/Sperrfristen.php
@@ -3,40 +3,31 @@
 //This Script sets the unpublished Documents to published.
 //The script runs as a cron job.
 // Bootstrapping
-require_once dirname(__DIR__) . ('/common/bootstrap.php');
+require_once dirname(__FILE__) . ('/common/bootstrap.php');
 
 $docFinder = new Opus_DocumentFinder();
 $docFinder->setServerState('unpublished');
+foreach ($docFinder->ids() as $id) {
+    $doc = null;
 
-foreach ($docFinder->ids() as $id)
-{
-
-    $d = null;
-    try
-    {
-        $d = new Opus_Document($id);
-        $originalDate = $d->getCompletedDate();
-
+    try {
+        $doc = new Opus_Document($id);
+        $originalDate = $doc->getCompletedDate();
     }
-    catch (Opus_Model_NotFoundException $e)
-    {
+    catch (Opus_Model_NotFoundException $e) {
         // document with id $id does not exist
         continue;
     }
-
-    if (!is_null($d))
-    {
+    if (! is_null($doc)) {
         $date = new Opus_Date();
         $newDate = $date->setNow();
-        if ($originalDate <= $newDate)
-        {
-            $d->setServerState('published');
-            $d->setServerDatePublished($date);
-            $d->store();
+        if ($originalDate == $newDate) {
+            $doc->setServerState('published');
+            $doc->setServerDatePublished($date);
+            $doc->store();
             echo "publishing of document with id $id was successful\n";
         }
     }
 }
-
 echo "done.\n";
 exit();
