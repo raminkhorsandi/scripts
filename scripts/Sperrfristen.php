1cd3703c5b5f601ef64e5ca1a5207306773a4730
NEWUSER <ramin.khorsandi.90@gmail.com>
2019-09-27 13:38:11 +0200

    Revert "OPUSVIER-4152 modified the coding Style"
    This reverts commit 864ee2046df86fadc759e5db4555f8a78a6e1b42.

--- scripts/Sperrfristen.php
+++ scripts/Sperrfristen.php
@@ -1,35 +1,42 @@
-
 #!/usr/bin/env php
 <?php
 //This Script sets the unpublished Documents to published.
 //The script runs as a cron job.
 // Bootstrapping
-require_once dirname(__FILE__) . ('/common/bootstrap.php');
+require_once dirname(__DIR__) . ('/common/bootstrap.php');
 
 $docFinder = new Opus_DocumentFinder();
 $docFinder->setServerState('unpublished');
-foreach ($docFinder->ids() as $id) {
-    $doc = null;
 
-    try {
-        $doc = new Opus_Document($id);
-        $originalDate = $doc->getCompletedDate();
+foreach ($docFinder->ids() as $id)
+{
+
+    $d = null;
+    try
+    {
+        $d = new Opus_Document($id);
+        $originalDate = $d->getCompletedDate();
+
     }
-    catch (Opus_Model_NotFoundException $e) {
+    catch (Opus_Model_NotFoundException $e)
+    {
         // document with id $id does not exist
         continue;
     }
 
-    if (! is_null($doc)) {
+    if (!is_null($d))
+    {
         $date = new Opus_Date();
         $newDate = $date->setNow();
-        if ($originalDate == $newDate) {
-            $doc->setServerState('published');
-            $doc->setServerDatePublished($date);
-            $doc->store();
+        if ($originalDate <= $newDate)
+        {
+            $d->setServerState('published');
+            $d->setServerDatePublished($date);
+            $d->store();
             echo "publishing of document with id $id was successful\n";
         }
     }
 }
+
 echo "done.\n";
 exit();
